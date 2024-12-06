e9dedad2a7a2136a2959e15569a217da8fa73748
Hoang Bao <ngocbao1796@gmail.com>
2024-12-06 09:54:17 +0700

    [update] change param buy proxy

--- src/enums/proxy.enum.ts
+++ src/enums/proxy.enum.ts
@@ -7,8 +7,8 @@ export enum ProxyType {
 
 export const ProxyTypeMapping: Record<string, string> = {
     "2b1701e2-f6d4-4a29-82f0-7dcb0c820f53": "Viettel",
-    "3c015121-6f5b-4c1b-9abe-4c18362dedd2": "Fpt",
+    "3c015121-6f5b-4c1b-9abe-4c18362dedd2": "FPT",
     "90438f46-835c-4e24-be9f-6564760d3490": "US",
-    "c238ce00-9123-4428-8990-46eee8e3d2ff": "Vnpt",
-    "cac02a2b-8ef5-4052-ae7e-45bbe0ef5e23": "Datacenter",
+    "c238ce00-9123-4428-8990-46eee8e3d2ff": "VNPT",
+    "cac02a2b-8ef5-4052-ae7e-45bbe0ef5e23": "DatacenterB",
 };
