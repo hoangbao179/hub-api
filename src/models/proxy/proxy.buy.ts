50972a59f8658333ada1987e88662df49da82ecd
Hoang Bao <ngocbao1796@gmail.com>
2025-02-26 10:01:00 +0700

    [update] inventory

--- src/models/proxy/proxy.buy.ts
+++ src/models/proxy/proxy.buy.ts
@@ -20,3 +20,27 @@ export interface SuccessResponse {
     status: number;
     comen: string;
 }
+
+
+export interface Attribute {
+    key: string;
+    modified?: number;
+    by_type?: string;
+    datetime?: string;
+    text?: string;
+    by?: string;
+    user_value?: string;
+    boolean?: boolean;
+  }
+  
+  export interface Lead {
+    id: string;
+    account_id: string;
+    attributes: Attribute[];
+    type: string;
+    profile_id: string;
+    channel_source: string;
+    channel: string;
+    created: number;
+    updated: number;
+  }
